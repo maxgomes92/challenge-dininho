acc8826e792cb5db4e46274e36f7e76a679158e5
Max Gomes <maximiliano.pinto@klarna.com>
2020-04-02 01:41:11 +0200

    Add dininho

--- js/main.js
+++ js/main.js
@@ -1,27 +1,47 @@
 
+let gameSpeed = 100
+let totalTime = 0
+
 const ground = {
   x: 0,
   y: 0,
   img: null,
-  speed: 10,
+  speed: gameSpeed * 4,
 }
 
 const sky = {
   x: 0,
   y: 0,
   img: null,
-  speed: 2,
+  speed: gameSpeed,
+}
+
+const player = {
+  x: null,
+  y: null,
+  speed: gameSpeed * 4,
+  runningImgs: [],
 }
 
 function preload () {
   ground.img = loadImage('assets/ground.png');
   sky.img = loadImage('assets/sky.png');
+
+  player.runningImgs = [
+    loadImage('assets/player/running/running_1.png'),
+    loadImage('assets/player/running/running_2.png'),
+    loadImage('assets/player/running/running_3.png'),
+    loadImage('assets/player/running/running_4.png'),
+  ]
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   ground.y = windowHeight - ground.img.height;
+  
+  player.x = windowWidth * 0.2;
+  player.y = windowHeight - 240;
 }
 
 function draw() {
@@ -33,18 +53,22 @@ function draw() {
   drawBackground(ground);
   ground.x = updateBackgroundPosition(ground);
 
-  animatePlayer();
+  animatePlayer(player);
 }
 
-function animatePlayer() {
+function animatePlayer({x, y, runningImgs, speed}) {
+  totalTime += deltaTime / 1000
 
+  const step = 80 / speed
+  const imgIndex = Math.round((totalTime / step) % 3) // Returns 0 to 3
+  image(runningImgs[imgIndex], x, y)
 }
 
 function updateBackgroundPosition({x, img, speed}) {
   if (Math.abs(x) >= img.width) {
-    return 0
+    return img.width + x
   } else {
-    return x - speed;
+    return (x - (speed * (deltaTime / 1000)));
   }
 }
 
@@ -59,5 +83,6 @@ function drawBackground ({img, x, y}) {
 }
 
 function windowResized() {
+  ground.y = windowHeight - ground.img.height;
   resizeCanvas(windowWidth, windowHeight);
 }
