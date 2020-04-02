e1e39a9e32587b6f3cf69d630e1b26f052220729
Max Gomes <maximiliano.pinto@klarna.com>
2020-04-02 03:09:57 +0200

    Make it jump

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 let gameSpeed = 100
 let totalTime = 0
 
+const SPACE_BAR = 32
+
 const ground = {
   x: 0,
   y: 0,
@@ -21,6 +23,14 @@ const player = {
   y: null,
   speed: gameSpeed * 4,
   runningImgs: [],
+  risingImg: null,
+  fallingImg: null,
+
+  // jump related
+  isRising: false,
+  isFalling: false,
+  jumpingTime: 0,
+  startingY: 0,
 }
 
 function preload () {
@@ -33,6 +43,9 @@ function preload () {
     loadImage('assets/player/running/running_3.png'),
     loadImage('assets/player/running/running_4.png'),
   ]
+
+  player.risingImg = loadImage('assets/player/jumping/rising.png')
+  player.fallingImg = loadImage('assets/player/jumping/falling.png')
 }
 
 function setup() {
@@ -43,6 +56,9 @@ function setup() {
 function draw() {
   background(255);
 
+  // Updates total time
+  totalTime += deltaTime / 1000
+
   drawBackground(sky);
   sky.x = updateBackgroundPosition(sky);
 
@@ -52,18 +68,69 @@ function draw() {
   animatePlayer(player);
 }
 
+function keyPressed() {
+  switch (keyCode) {
+    case SPACE_BAR:
+      handleJump()
+      break;
+    default:
+  }
+}
+
+function handleJump() {
+  if (player.isFalling || player.isRising) return;
+
+  player.isRising = true;
+  player.jumpingTime = 0;
+  player.startingY = player.y;
+}
+
 function setupPositioning() {
   ground.y = windowHeight - ground.img.height;
   player.x = windowWidth * 0.2;
   player.y = windowHeight - 240;
 }
 
-function animatePlayer({x, y, runningImgs, speed}) {
-  totalTime += deltaTime / 1000
+function animatePlayer() {
+  const {
+    x, y,
+    runningImgs,
+    speed,
+    isRising,
+    isFalling,
+    jumpingTime,
+  } = player
 
-  const step = 80 / speed
-  const imgIndex = Math.round((totalTime / step) % 3) // Returns 0 to 3
-  image(runningImgs[imgIndex], x, y)
+  if (isRising || isFalling) {
+    // (-50x^2)+30x
+    const currentY = player.y
+    const height = -50*Math.pow(jumpingTime,2) + 30*jumpingTime;
+    const newY = player.startingY - height * 50;
+
+    // Detect if falling
+    if (newY > currentY) {
+      player.isRising = false;
+      player.isFalling = true;
+    }
+
+    // Detect end of jump
+    if (newY > player.startingY) {
+      player.isFalling = false;
+      player.y = player.startingY;
+      image(runningImgs[0], x, player.y);
+    } else {
+      player.y = newY;
+
+      const img = isRising ? player.risingImg : player.fallingImg
+      image(img, x, player.y);
+    }
+
+    player.jumpingTime += deltaTime / 1000
+  } else {
+    const step = 80 / speed
+    const imgIndex = Math.round((totalTime / step) % 3) // Returns 0 to 3
+    image(runningImgs[imgIndex], x, y)
+  }
 }
 
 function updateBackgroundPosition({x, img, speed}) {
